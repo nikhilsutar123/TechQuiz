dbfd1ffbc5a1571d573cdd484575de980fd7917d
nikhilsutar123 <nkhlstr@gmail.com>
2019-03-27 21:48:25 +0530

    add checkScore() to calculate final score

--- app/src/main/java/com/example/android/techquiz/MainActivity.java
+++ app/src/main/java/com/example/android/techquiz/MainActivity.java
@@ -3,33 +3,42 @@ package com.example.android.techquiz;
 import android.annotation.TargetApi;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 @TargetApi(19)
 public class MainActivity extends AppCompatActivity {
 
+    //AlertDialog.Builder dialog;
+    AlertDialog d;
+    Boolean isShowingDialog = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         final AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this)
-                .setMessage(getString(R.string.rule1) + "\n\n" + getString(R.string.rule2) + "\n\n" + getString(R.string.rule3))
-                .setTitle(R.string.rules)
+        if (savedInstanceState != null) {
+            isShowingDialog = savedInstanceState.getBoolean("IS_SHOWING_DIALOG", false);
+            if (isShowingDialog) {
+                final AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setMessage(getString(R.string.rule1) + "\n\n" + getString(R.string.rule2) + "\n\n" + getString(R.string.rule3))
+                        .setTitle(R.string.rules)
+                        .setPositiveButton(R.string.okButton, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
 
-                .setPositiveButton(R.string.okButton, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        
-                    }
-                });
-        final AlertDialog d = dialog.create();
-        d.show();
+                            }
+                        });
+                d = dialog.create();
+                d.show();
+
+                isShowingDialog = true;
+            }
+        }
         Button playButton = findViewById(R.id.play_button);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,21 +51,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //code to hide navbar
-    public void immersiveMode() {
-        View decor = getWindow().getDecorView();
-        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-    }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        immersiveMode();
+        final View decor = getWindow().getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         getWindow().getDecorView().setOnSystemUiVisibilityChangeListener
                 (new View.OnSystemUiVisibilityChangeListener() {
                     @Override
                     public void onSystemUiVisibilityChange(int visibility) {
-                        immersiveMode();
+                        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
                     }
                 });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (d != null && d.isShowing()) {
+            d.dismiss();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean("IS_SHOWING_DIALOG", isShowingDialog);
+        super.onSaveInstanceState(outState);
     }
 }
--- app/src/main/java/com/example/android/techquiz/QuizLogic.java
+++ app/src/main/java/com/example/android/techquiz/QuizLogic.java
@@ -5,50 +5,110 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 @TargetApi(19)
 public class QuizLogic extends AppCompatActivity {
-     int score;
+    int score;
+
+    RadioButton rb1;
+    RadioButton rb2;
+    String et1, et2;
+    CheckBox jpeg, png, tif, spacex, tesla;
+
+    EditText ram;
+    EditText bit;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quiz_screen);
         onWindowFocusChanged(false);
 
-        RadioGroup rg1 = findViewById(R.id.radioGroup_1);
-        final RadioButton rb1 = findViewById(R.id.steve);
-               rg1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-           @Override
-           public void onCheckedChanged(RadioGroup radioGroup, int i) {
-               if (rb1.isChecked()){
-                   score++;
-               }
+        Button result = findViewById(R.id.submit);
 
-           }
-       });
+        result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkScore();
+                Toast.makeText(getApplicationContext(), "Your Final score is " + score + " out of 10", Toast.LENGTH_SHORT).show();
+                if (rb1.isSelected() || rb2.isSelected() ||
+                        spacex.isChecked() || tesla.isChecked() || jpeg.isChecked() || png.isChecked() || tif.isChecked()
+                        || ram.getText() != null || bit.getText() != null) {
+                    rb1.setChecked(false);
+                    rb2.setChecked(false);
+                    spacex.toggle();
+                    tesla.toggle();
+                    jpeg.toggle();
+                    png.toggle();
+                    tif.toggle();
+                    ram.setText("");
+                    bit.setText("");
+
+                    et1 = "";
+                    et2 = "";
+
+                    score=0;
+                }
+            }
+        });
     }
 
-    public void seeScore(View view) {
-        Toast.makeText(getApplicationContext(), "Your final score is "+score+ " out of 10", Toast.LENGTH_LONG).show();
-    }
+    public void checkScore() {
 
-    public void immersiveMode() {
-        View decor = getWindow().getDecorView();
-        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        rb1 = findViewById(R.id.steve);
+        rb2 = findViewById(R.id.moto);
+
+        spacex = findViewById(R.id.c1);
+        tesla = findViewById(R.id.c4);
+        jpeg = findViewById(R.id.c21);
+        png = findViewById(R.id.c23);
+        tif = findViewById(R.id.c24);
+
+        if (rb1.isChecked()) {
+            score++;
+        }
+        if (rb2.isChecked()) {
+            score++;
+        }
+        if (spacex.isChecked() && tesla.isChecked()) {
+            score = score + 2;
+        } else {
+            Toast.makeText(getApplicationContext(), "2 Answers in Q3! You get no points for this question!", Toast.LENGTH_LONG).show();
+        }
+        if (jpeg.isChecked() && png.isChecked() && tif.isChecked()) {
+            score = score + 2;
+        } else {
+            Toast.makeText(getApplicationContext(), "3 Answers in Q4! You get no points for this question!", Toast.LENGTH_LONG).show();
+        }
+
+        ram = findViewById(R.id.edit1);
+        bit = findViewById(R.id.edit2);
+
+        et1 = ram.getText().toString();
+        et2 = bit.getText().toString();
+
+        if (et1.equalsIgnoreCase("random access memory"))
+            score = score + 2;
+        if (et2.equals("1") || et2.equalsIgnoreCase("1 byte") || et2.equalsIgnoreCase("one"))
+            score = score + 2;
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        immersiveMode();
+        final View decor = getWindow().getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         getWindow().getDecorView().setOnSystemUiVisibilityChangeListener
                 (new View.OnSystemUiVisibilityChangeListener() {
                     @Override
                     public void onSystemUiVisibilityChange(int visibility) {
-                        immersiveMode();
+                        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
                     }
                 });
     }
